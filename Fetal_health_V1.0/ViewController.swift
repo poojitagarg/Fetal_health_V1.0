774c1d9511a0d712f5ee6205776d44a549e71cc6
Poojita Garg <poojita8garg@gmail.com>
2023-02-06 18:00:43 -0500

    Initial Commit

---
+++ Fetal_health_V1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetal_health_V1.0
+//
+//  Created by School on 2/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
